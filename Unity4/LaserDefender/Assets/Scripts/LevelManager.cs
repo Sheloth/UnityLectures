8b99dac0ddb98cfe95e1d05f76aeda73a262d69e
Anton Karpinskyi <anton.karpinsky@gmail.com>
2016-10-22 16:28:52 +0200

    LaserDefender project was created

---
+++ Unity4/LaserDefender/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+	
+	public void LoadLevel (string name) {
+		Debug.Log ("LoadLevel called. Param: " + name);
+		Application.LoadLevel(name);
+	}
+
+	public void Quit () {
+		Debug.Log ("Quit called");
+		Application.Quit();
+	}
+	
+	public void LoadNextLevel () {
+		Application.LoadLevel(Application.loadedLevel + 1);	
+	}
+}
