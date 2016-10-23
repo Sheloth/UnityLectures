3e26c1add00e35d6f6f7126411afaa4edbecd87c
Anton Karpinskyi <anton.karpinsky@gmail.com>
2016-10-23 09:38:43 +0200

    LaserDefender : Enemy formation movement

--- Unity4/LaserDefender/Assets/Scripts/EnemySpawner.cs
+++ Unity4/LaserDefender/Assets/Scripts/EnemySpawner.cs
@@ -4,9 +4,22 @@ using System.Collections;
 public class EnemySpawner : MonoBehaviour {
 	
 	public GameObject enemy;
-
+	public float speed=3f;
+	public float width = 10f;
+	public float height = 5f;
+	
+	private bool isMovingRight = false;
+	private float minX;
+	private float maxX;
+	
 	// Use this for initialization
 	void Start () {
+		float distanceToCamera = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftBoundary = Camera.main.ViewportToWorldPoint(new Vector3(0,0,distanceToCamera));
+		Vector3 rightBoundary = Camera.main.ViewportToWorldPoint(new Vector3(1,0,distanceToCamera));
+		minX = leftBoundary.x;
+		maxX = rightBoundary.x;
+	
 		foreach(Transform child in transform)
 		{
 			GameObject enemyInstance = Instantiate(enemy, child.position, Quaternion.identity) as GameObject;
@@ -16,6 +29,23 @@ public class EnemySpawner : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		MoveFormation();
+	}
 	
+	void MoveFormation () {
+		if(isMovingRight) {
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		}
+		else {
+			transform.position += Vector3.left * speed * Time.deltaTime;
+		}
+		
+		if((transform.position.x <= (minX + width*0.5f)) || (transform.position.x >= (maxX - width*0.5f))) {
+			isMovingRight = !isMovingRight;
+		}
+	}	
+	
+	void OnDrawGizmos () {
+		Gizmos.DrawWireCube(transform.position, new Vector3(width, height, 0));
 	}
 }
