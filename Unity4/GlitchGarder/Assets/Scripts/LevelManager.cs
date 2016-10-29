f7014bed10d1d9fe2c321f31a3ce5496897b1b17
Anton Karpinskyi <anton.karpinsky@gmail.com>
2016-10-29 19:28:32 +0200

    GlitchGarder : - Splash screen - Assets - Start screen

---
+++ Unity4/GlitchGarder/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+	
+	public float loadLevelDelay;
+	
+	void Start () {
+		if(loadLevelDelay != 0) {
+			Invoke("LoadNextLevel", loadLevelDelay);
+		}
+	}
+	
+	public void LoadLevel (string name) {
+		Debug.Log ("LoadLevel called. Param: " + name);
+		Application.LoadLevel(name);
+	}
+
+	public void Quit () {
+		Debug.Log ("Quit called");
+		Application.Quit();
+	}
+	
+	public void LoadNextLevel () {
+		Application.LoadLevel(Application.loadedLevel + 1);	
+	}
+}
