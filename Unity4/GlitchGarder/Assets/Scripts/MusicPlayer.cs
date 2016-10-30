a920681b3423d39da45cc45722c6d5bad2fb6326
Anton Karpinskyi <anton.karpinsky@gmail.com>
2016-10-30 17:39:17 +0100

    GlitchGarden : basic scenes navigation

---
+++ Unity4/GlitchGarder/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+
+	public AudioClip[] levelMusicArray;
+
+	private AudioSource audioSource;
+
+	void Awake () {
+		DontDestroyOnLoad(gameObject);
+	}
+
+	// Use this for initialization
+	void Start () {
+		audioSource = GetComponent<AudioSource>();
+	}
+	
+	void OnLevelWasLoaded (int level) {
+		AudioClip currentLevelMusic = levelMusicArray[level];
+		
+		if(currentLevelMusic) {
+			audioSource.clip = currentLevelMusic;
+			audioSource.loop = true;
+			audioSource.Play();
+		}
+	}
+}
