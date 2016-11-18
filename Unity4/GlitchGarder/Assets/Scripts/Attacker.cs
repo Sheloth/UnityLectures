f1730e742c84700dd8928850b1e2ed913bb1da3d
Anton Karpinskyi <anton.karpinsky@gmail.com>
2016-11-18 23:44:00 +0100

    GlitchGarden: - Attackers animations flow - Health component

---
+++ Unity4/GlitchGarder/Assets/Scripts/Attacker.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attacker : MonoBehaviour {
+
+	private float currentSpeed;
+	private GameObject currentTarget;
+	private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector3.left * currentSpeed * Time.deltaTime);
+		if(!currentTarget) {
+			animator.SetBool("isAttacking", false);
+		}
+	}
+	
+	void OnTriggerEnter2D() {
+		Debug.Log(name + " trigger entered");
+	}
+	
+	public void SetSpeed (float speed) {
+		currentSpeed = speed;
+		print(currentSpeed);
+	}
+	
+	public void StrikeCurrentTarget (float damage) {
+		if(currentTarget) {
+			Health health = currentTarget.GetComponent<Health>();
+			
+			if(health) {
+				health.DealDamage(damage);
+			}
+		}
+	}
+	
+	public void Attack (GameObject obj) {
+		currentTarget = obj;
+	}
+}
--- Unity4/GlitchGarder/Assets/Scripts/Defender.cs
+++ Unity4/GlitchGarder/Assets/Scripts/Defender.cs
@@ -1,10 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Attackers : MonoBehaviour {
-
-	[Range (-1f, 1.5f)]
-	public float walkSpeed;
+public class Defender : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
@@ -13,6 +10,10 @@ public class Attackers : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Translate(Vector3.left * walkSpeed * Time.deltaTime);
+	
+	}
+	
+	void OnTriggerEnter2D() {
+		Debug.Log(name + " trigger entered");
 	}
 }
