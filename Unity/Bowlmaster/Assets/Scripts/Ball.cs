0bc4bae146d37b0c02dd530aba192929edf45735
Anton Karpinskyi <anton.karpinsky@gmail.com>
2016-11-28 21:51:04 +0100

    Bowlmaster: - Initial commit - Floor was added - Pins were added

---
+++ Unity/Bowlmaster/Assets/Scripts/Ball.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+    public float launchSpeed = 1;
+
+    private Rigidbody ballRigidBody;
+    private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start ()
+    {
+        ballRigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+
+        Launch();
+    }
+
+    private void Launch()
+    {
+        ballRigidBody.velocity = new Vector3(0, 0, launchSpeed);
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
