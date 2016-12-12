eb3d4ddbdedacd0f7627f5010a51472e6ad2714c
Anton Karpinskyi <anton.karpinsky@gmail.com>
2016-12-12 23:11:45 +0100

    Bowlmaster : - ActionMaster refactoring - Scoredisplay tests - Starting bowl position constraints

--- Unity/Bowlmaster/Assets/Scripts/ActionMaster.cs
+++ Unity/Bowlmaster/Assets/Scripts/ActionMaster.cs
@@ -8,73 +8,45 @@ public class ActionMaster {
         Tidy,
         Reset,
         EndTurn,
-        EndGame
+        EndGame,
+        Undefined
     }
 
-    private int[] bowls = new int[21];
-    private int bowl = 1;
-
-    public static Action NextAction(List<int> pinFalls) {
-        ActionMaster actionMaster = new ActionMaster();
-        Action currentAction = new Action();
-
-        foreach(int pinFall in pinFalls) {
-            currentAction = actionMaster.Bowl(pinFall);
-        }
-
-        return currentAction;
-    }
-
-    private Action Bowl (int pins) {
-        if(pins < 0 || pins > 10) {
-            throw new UnityException("Invalid pins amount");
-        }
-
-        bowls[bowl - 1] = pins;
-
-        if(bowl >= 21) {
-            return Action.EndGame;
-        }
-
-        if (bowl == 19 && pins == 10) {
-            bowl += 1;
-            return Action.Reset;
-        }
-        else if(bowl == 20) {
-            bowl++;
-            if (IsSpareInLastFrame() && pins != 0) {
-                return Action.Reset;
+    public static Action NextAction(List<int> rolls) {
+        Action nextAction = Action.Undefined;
+        for (int i = 0; i < rolls.Count; ++i) {
+            if (i == 20) {
+                nextAction = Action.EndGame;
             }
-            else if(Bowl21Awarded()){
-                return Action.Tidy;
+            else if (i >= 18 && rolls[i] == 10) {
+                nextAction = Action.Reset;
             }
-            else {
-                return Action.EndGame;
+            else if (i == 19) {
+                if (rolls[18] == 10 && rolls[19] < 10) {
+                    nextAction = Action.Tidy;
+                }
+                else if (rolls[18] + rolls[19] == 10) {
+                    nextAction = Action.Reset;
+                }
+                else {
+                    nextAction = Action.EndGame;
+                }
+            }
+            else if (i % 2 == 0) { // First bowl of frame
+                if (rolls[i] == 10) {
+                    rolls.Insert(i, 0); // Insert virtual 0 after strike
+                    nextAction = Action.EndTurn;
+                }
+                else {
+                    nextAction = Action.Tidy;
+                }
+            }
+            else { // Second bowl of frame
+                nextAction = Action.EndTurn;
             }
         }
 
-        if(pins == 10) {
-            bowl += (bowl % 2 == 0 ? 1 : 2);
-            return Action.EndTurn;
-        }
-
-        if(bowl % 2 != 0) {
-            bowl += 1;
-            return Action.Tidy;
-        }
-        else {
-            bowl += 1;
-            return Action.EndTurn;
-        }
-
-        throw new UnityException("Invalid ActionMaster state");
+        return nextAction;
     }
 
-    private bool IsSpareInLastFrame() {
-        return (bowls[19 - 1] + bowls[20 - 1]) % 10 == 0;
-    }
-
-    private bool Bowl21Awarded() {
-        return (bowls[19-1] + bowls[20-1] >= 10);
-    }
 }
---
+++ Unity/Bowlmaster/Assets/Scripts/ScoreDisplay.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ScoreDisplay : MonoBehaviour {
+
+    public Text[] rollsTexts;
+    public Text[] framesTexts;
+
+    void Start () {
+        foreach(Text frame in framesTexts) {
+            frame.text = "";
+        }
+
+        foreach (Text roll in rollsTexts) {
+            roll.text = "";
+        }
+    }
+
+    static public string FormatRolls(List<int> rolls) {
+        string output = "";
+
+        for (int i = 0; i < rolls.Count; i++) {
+            int box = output.Length + 1;
+
+            
+            if (rolls[i] == 0) { // Zero in frame
+                output += "-";
+            }
+            else if ((box % 2 == 0 || box == 21) && rolls[i - 1] + rolls[i] == 10) { // Spare
+                output += "/";
+            } 
+            else if (box >= 19 && rolls[i] == 10) { // Strike in 10 f
+                output += "X";
+            }
+            else if (rolls[i] == 10) { // Strike in frames 1-9
+                output += "X ";
+            }
+            else {
+                output += rolls[i].ToString();
+            }
+        }
+
+        return output;
+    }
+
+    public void FillFrames(List<int> frames) {
+        for (int i = 0; i < frames.Count; ++i) {
+            framesTexts[i].text = frames[i].ToString();
+        }
+    }
+
+    public void FillRolls (List<int> rolls) {
+        string scoresString = FormatRolls(rolls);
+        for(int i = 0; i < scoresString.Length; i++) {
+            rollsTexts[i].text = scoresString[i].ToString();
+        }
+    }
+}
